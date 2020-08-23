2ebe03b1ab98d31abf43021015b60f82d63094ab
CHChang <holybible934@gmail.com>
2020-08-23 15:19:00 +0800

    Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).

---
+++ BestTimetoBuyandSell.c
@@ -0,0 +1,13 @@
+//
+// Created by CHANGCHUNGHUAN on 2020/8/23.
+//
+/* Say you have an array prices for which the ith element is the price of a given stock on day i.
+
+   Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+   Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+   */
+
+int maxProfit(int* prices, int pricesSize){
+
+}
