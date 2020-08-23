b7c84f41a5d7bb4d9da0c14cec6ca2371fe12f4b
CHChang <holybible934@gmail.com>
2020-08-23 22:19:28 +0800

    Use qsort to sort every string in strs and save them as Pair Categorize strings by 'pairArray.sorted' later

--- GroupAnagrams.c
+++ GroupAnagrams.c
@@ -2,6 +2,25 @@
 // Created by CHANGCHUNGHUAN on 2020/8/23.
 //
 /* Given an array of strings, group anagrams together. */
-char *** groupAnagrams(char ** strs, int strsSize, int* returnSize, int** returnColumnSizes){
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+    char* orig;
+    char* sorted;
+} Pair;
+
+int cmpChar(const void* a, const void* b) {
+    return *(const char*)a - *(const char*)b;
+}
+
+char *** groupAnagrams(char ** strs, int strsSize, int* returnSize, int** returnColumnSizes){
+    Pair* pairArray = malloc(sizeof(Pair)*strsSize);
+    for (int i = 0;i < strsSize; i++) {
+        char* sorted_str = malloc(sizeof(char)*strlen(strs[i]+1));
+        strcpy(sorted_str, strs[i]);
+        qsort(sorted_str, strlen(sorted_str), sizeof(char), cmpChar)
+        pairArray[i].orig = strs[i];
+        pairArray[i].sorted = sorted_str;
+    }
 }
