45c9338d957e963265ed98d9a9f74d5dc4b5fe94
CHChang <holybible934@gmail.com>
2020-08-23 16:34:18 +0800

    Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).

--- BestTimetoBuyandSell.c
+++ BestTimetoBuyandSell.c
@@ -9,5 +9,11 @@
    */
 
 int maxProfit(int* prices, int pricesSize){
-
+    int total = 0;
+    for (int i = 0; i + 1 < pricesSize; i++) {
+        if (prices[i] < prices[i+1]) {
+            total += prices[i+1] - prices[i];
+        }
+    }
+    return total;
 }
